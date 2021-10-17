d1a2985539a3f7db4dba5b497ce26c77170083ac
Asim <asim.awan@outlook.com>
2021-10-17 13:26:31 +0000

    Add reservation form and functionality to make new reservations

--- LightBnB_WebApp-master/public/javascript/index.js
+++ LightBnB_WebApp-master/public/javascript/index.js
@@ -2,5 +2,12 @@ $(() => {
   getAllListings().then(function( json ) {
     propertyListings.addProperties(json.properties);
     views_manager.show('listings');
+
+    $('.reserve-button').on('click', function() {
+      // const idData = $(this).attr('id');
+      // console.log(idData);
+      const idData = $(this).attr('id').substring(17);
+      views_manager.show('newReservation', idData);
+    })
   });
 });
